862aa2ac86105fecd5bc6f64ede83cd8f646ecad
ksrathore <kalyan@hungerbox.com>
2021-05-11 00:50:36 +0530

    first commit

---
+++ src/components/DashCards.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-card class="mx-auto" style="padding: 8px 12px;" max-width="80%">
+    <v-row dense>
+      <v-col v-for="(item, i) in items"
+          :key="i" cols="4">
+        <v-card style="margin: 12px;"
+            :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="text-h5" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                    v-if="item.artist === 'Ellie Goulding'"
+                    class="ml-2 mt-3"
+                    fab
+                    icon
+                    height="40px"
+                    right
+                    width="40px"
+                >
+                  <v-icon>mdi-play</v-icon>
+                </v-btn>
+
+                <v-btn
+                    v-else
+                    class="ml-2 mt-5"
+                    outlined
+                    rounded
+                    small
+                >
+                  START RADIO
+                </v-btn>
+              </v-card-actions>
+            </div>
+
+            <v-avatar
+                class="ma-3"
+                size="125"
+                tile
+            >
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'DashCards',
+  data: () => ({
+    items: [
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Day',
+        artist: 'Ellie Goulding',
+      },
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Day',
+        artist: 'Ellie Goulding',
+      },
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Day',
+        artist: 'Ellie Goulding',
+      },
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Day',
+        artist: 'Ellie Goulding',
+      },
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+    ],
+  }),
+}
+</script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <DashCards/>
+</template>
+
+<script>
+import DashCards from "@/components/DashCards";
+export default {
+  name:'Dashboard',
+  components: {DashCards},
+  props:{
+    title:String
+  }
+}
+</script>
